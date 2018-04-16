9181749fcf9c08e37e81c4285657f3f18778d3a4
lbegazo <luis_begazo@hotmail.es>
2018-04-16 18:46:55 -0500

    Update solution, new poker component
    Update solution, new poker component

---
+++ PokerGame/ClientApp/app/components/poker-form/poker-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poker-form',
+  templateUrl: './poker-form.component.html',
+  styleUrls: ['./poker-form.component.css']
+})
+export class PokerFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
